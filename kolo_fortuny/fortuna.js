cf42d031dbf7bebc216889347a8511a13839beca
MavoSV8 <lukaszpasi@gmail.com>
2021-11-07 13:43:36 +0100

    added background

--- kolo_fortuny/fortuna.js
+++ kolo_fortuny/fortuna.js
@@ -3,32 +3,41 @@ var game = {
   zdobyte : 0,
   zycia : 1,
 }
-alert(data[0]['country']);
+console.log(data[0]['country']);
 var elem = document.getElementById("panstwa");
 elem.innerHTML =data[0]['country'];
 
-alert(data.length);
-alert(data[0]['country'][2]);
+console.log(data.length);
+console.log(data[0]['country'][2]);
 
  for (var i = 0; i < data[0]['country'].length; i += 1) {
-    alert(data[0]['country'][i]);  
+   console.log(data[0]['country'][i]);
   }
 
 
 addElement("wrap");
 //LISTENERS
 
-document.getElementById("graj").addEventListener("click", Sprawdz_Litery); 
-alert(game.zycia);
+document.getElementById("graj").addEventListener("click", Sprawdz_Litery);
+console.log(game.zycia);
 
 
 //FUNKCJE
 function Sprawdz_Litery(){
   var liter = document.getElementById("wpisz_litere").value;
-  alert(liter);
-  alert(getRandomInt(10,20));
+  console.log(liter);
+  console.log(getRandomInt(10,20));
 }
+function StartGame(starterDiv,autorDiv){
+  startDiv = document.createElement("span");
+  authorDiv = document.getElementById(gameDiv);
+  document.body.insertBefore(newDiv, my_div);
 
+  newDiv2 = document.createElement("span");
+  newDiv2.innerHTML = "jasiokotek2";
+  document.body.insertBefore(newDiv2, my_div.nextSibling);
+
+}
 
 function addElement(mydiv)
 {
